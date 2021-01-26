d3e78f90c38127e7cbcd2d63a8d8226c2f6f2e8c
Matt-0815 <1163534503@qq.com>
2021-01-26 17:42:45 +0800

    nicai

---
+++ jump7.py
@@ -0,0 +1,13 @@
+shiyanlou:~/ $ ls                                                    [17:31:55]
+anaconda3  Code  Desktop
+shiyanlou:~/ $ cd Code                                               [17:32:04]
+shiyanlou:Code/ $ git clone https://github.com/Matt-0815/shiyanlou-code.git
+正克隆到 'shiyanlou-code'...
+warning: 您似乎克隆了一个空仓库。
+检查连接... 完成。
+shiyanlou:Code/ $ ls                                                 [17:33:49]
+shiyanlou-code
+shiyanlou:Code/ $ cd shiyanloucode                                   [17:34:07]
+cd: 没有那个文件或目录: shiyanloucode
+shiyanlou:Code/ $ cd shiyanlou-code                                  [17:34:26]
+shiyanlou:shiyanlou-code/ (master) $ gedit jump7.py   
